6a4a6fe2ed2b7005b66ee26b9e3f4c56f4ffae9b
Monika Mazur <monika.mazurstr@gmail.com>
2018-07-14 15:32:28 +0200

    Test dodawania do klasy Calculator

---
+++ src/test/java/pl/sda/tdd/ParametrizedByJUnitParamsCalculatorTest.java
@@ -0,0 +1,27 @@
+package pl.sda.tdd;
+
+import junitparams.JUnitParamsRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnitParamsRunner.class)
+public class ParametrizedByJUnitParamsCalculatorTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void subtract() {
+    }
+
+    @Test
+    public void multiply() {
+    }
+
+    @Test
+    public void divide() {
+    }
+}
