266c7e86a90df1b9d5f46a6384ea6d67a45f1747
Monika Mazur <monika.mazurstr@gmail.com>
2018-07-14 12:39:44 +0200

    Kalkulator z metodami testującymi, również wyjątki

--- src/main/java/pl/sda/tdd/Calculator.java
+++ src/main/java/pl/sda/tdd/Calculator.java
@@ -1,4 +1,26 @@
 package pl.sda.tdd;
 
 public class Calculator {
+
+    public int add(int arg1, int arg2) {
+        return arg1 + arg2;
+    }
+
+    public int subtract(int arg1, int arg2) {
+        return arg1 - arg2;
+    }
+
+    public int multiply(int arg1, int arg2) {
+        return arg1 * arg2;
+    }
+
+    public double divide(double arg1, double arg2) {
+        if (arg2 == 0) {
+            throw new IllegalArgumentException("Nie dzieli się przez 0");
+        }
+        else {
+            return arg1 / arg2;
+        }
+    }
+
 }
--- src/test/java/pl/sda/tdd/CalculatorTest.java
+++ src/test/java/pl/sda/tdd/CalculatorTest.java
@@ -1,5 +1,45 @@
-import static org.junit.Assert.*;
+package pl.sda.tdd;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
+    private Calculator calc;
+
+    @Before
+    public void setup() {
+        calc = new Calculator();
+    }
+
+    @Test
+    public void addTest() {
+        int result = calc.add(7, 2);
+        assertEquals(9, result);
+    }
+
+    @Test
+    public void subtractTest() {
+        int result = calc.subtract(7, 2);
+            assertEquals(5, result);
+        }
+
+    @Test
+    public void multiplyTest() {
+        //int result = calc.multiply(7, 2);
+        assertEquals(14, calc.multiply(7, 2));
+    }
+
+    @Test
+    public void divideTest() {
+        double result = calc.divide(7, 2);
+        assertEquals(3.5, result, 0.5);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void divideZeroTest() {
+        calc.divide(7, 0);
+    }
 }
