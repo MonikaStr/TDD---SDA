c6a4218734d10042331347bfee7c073998b48cc1
Monika Mazur <monika.mazurstr@gmail.com>
2018-07-15 10:23:44 +0200

    AssertJ - dodatkowy test sprawdzający metodę multiply

--- src/main/java/pl/sda/tdd/MedianaFromTheTable.java
+++ src/main/java/pl/sda/tdd/MedianaFromTheTable.java
@@ -1,4 +1,30 @@
 package pl.sda.tdd;
 
 public class MedianaFromTheTable {
+    /*jeżeli tablica jest:
+    - nullem, to metoda zwraca Integera (a w teście rzutować go na inta)
+    - empty, to metoda zwraca 0
+    - even, to metoda zwraca liczbę z indeksem (i+1)
+     */
+
+    //jedna metoda z tablicą w parametrze
+
+    public static int showMedian(int[] tab){
+        int p = tab[(tab.length-1)/2];
+        return p;
+    }
+
 }
+/*
+w metodzie, z zajęć
+
+public Integer find(int[] tab) {
+if (tab == null || tab.length ==0) {
+return null;
+}
+if (tablength % 2 == 0) {
+return tab[tab.length - 1 / 2]
+}
+return tab[tab.length / 2];
+}
+ */
--- src/test/java/pl/sda/tdd/MedianaFromTheTableTest.java
+++ src/test/java/pl/sda/tdd/MedianaFromTheTableTest.java
@@ -1,5 +1,47 @@
+package pl.sda.tdd;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MedianaFromTheTableTest {
 
+    private int[] tabEmpty; //można to poprzenosić do metod
+    private int[] tabNull;
+    private int[] tabNotEven = {1, 2, 3};
+
+    @Test
+    public void showMedianEmptyTest() {
+        tabEmpty = new int[5];
+        int resultFromMethod = MedianaFromTheTable.showMedian(tabEmpty);
+        assertEquals(0, resultFromMethod);
+    }
+
+    @Test
+    public void showMedianNullTest() {
+       // int resultFromMethod = MedianaFromTheTable.showMedian(tabNull);
+       assertNull(MedianaFromTheTable.showMedian(null));
+    }
+
+    @Test
+    public void showMedianEvenTest() {
+        int[] tabEven = {1, 2, 3, 4};
+        int resultFromMethod = MedianaFromTheTable.showMedian(tabEven);
+        assertEquals(2, resultFromMethod);
+    }
+
+    @Test
+    public void showMedianNotEvenTest() {
+        int resultFromMethod = MedianaFromTheTable.showMedian(tabNotEven);
+        assertEquals(2, resultFromMethod);
+    }
+
 }
+/*
+Kod z zajęć
+@Test
+public void returnNullWhenArrayEmpty() {
+init[] emptyArray = new int[0];
+assertNull(MedianaFromTheTable.showMedian(emptyArray));
+ */
