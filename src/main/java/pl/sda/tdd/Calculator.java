5c96b2704627e8f9c786e3332030b4cc6ddebd82
Monika Mazur <monika.mazurstr@gmail.com>
2018-07-15 09:40:27 +0200

    dodana metoda sprawdzająca, czy pojawia się wyjątek, gdy argument jest nullem

--- src/main/java/pl/sda/tdd/Calculator.java
+++ src/main/java/pl/sda/tdd/Calculator.java
@@ -17,10 +17,23 @@ public class Calculator {
     public double divide(double arg1, double arg2) {
         if (arg2 == 0) {
             throw new IllegalArgumentException("Nie dzieli się przez 0");
-        }
-        else {
+        } else {
             return arg1 / arg2;
         }
+    }
+
+/*
+Rozszerz klasę Calculator tak, żeby przyjmowała wartości typu Integer
+zamiast int. Jeśli któryś z argumentów = null -> rzucamy wyjątek z
+odpowiednią wiadomością. Jeśli dzielnik = null -> rzucamy wyjątek z
+odpowiednią wartością.
+ */
+    public Integer add2(Integer arg1, Integer arg2) {
+
+        if (arg1 == null || arg2 == null) {
+            throw new NullPointerException("Parametr jest nullem");
+        }
+        return arg1 + arg2;
     }
 
 }
